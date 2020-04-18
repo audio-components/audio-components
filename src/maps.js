caf039fe09c7bae670868ebb4fd7a84b6deb1236
Gerard Abelló Serras <gerardabello@gmail.com>
2020-04-18 15:27:15 +0200

    feat: Add maps file

---
+++ src/maps.js
@@ -0,0 +1,55 @@
+export const applyLaw = (v, min, max, law) => {
+  let l = law || 'linear'
+  if (min >= max) {
+    throw new Error('min should be smaller than max')
+  }
+
+  let result = 0
+  switch (l) {
+    case 'linear':
+      result = v * (max - min) + min
+      break
+    case 'log':
+      if (min < 0) {
+        throw new Error('Called applyLaw with a log law and negative values')
+      }
+      result = min * Math.pow(max / min, v)
+      break
+    case 'pow':
+      result = min + v * v * (max - min)
+      break
+    default:
+      result = v * (max - min) + min
+      break
+  }
+
+  return result
+}
+
+export const reverseLaw = (v, min, max, law) => {
+  let l = law || 'linear'
+  if (min >= max) {
+    throw new Error('min should be smaller than max')
+  }
+
+  let result = 0
+  switch (l) {
+    case 'linear':
+      result = (v - min) / (max - min)
+      break
+    case 'log':
+      if (min < 0) {
+        throw new Error('Called applyLaw with a log law and negative values')
+      }
+      result = Math.log(v / min) / Math.log(max / min)
+      break
+    case 'pow':
+      result = Math.sqrt((v - min) / (max - min))
+      break
+    default:
+      result = (v - min) / (max - min)
+      break
+  }
+  return result
+}
+
